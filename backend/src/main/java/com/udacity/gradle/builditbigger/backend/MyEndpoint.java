3bf234c5dfafc1fdce236a3eecbcf1e44af89f09
suzanne <suzanne.aitchison@gmail.com>
2017-12-31 17:19:07 +0000

    get joke implemented in back end

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -9,6 +9,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.example.jokefactory.JokeProvider;
 
 import javax.inject.Named;
 
@@ -30,6 +31,13 @@ public class MyEndpoint {
         MyBean response = new MyBean();
         response.setData("Hi, " + name);
 
+        return response;
+    }
+
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke(){
+        MyBean response = new MyBean();
+        response.setData(new JokeProvider().getJoke());
         return response;
     }
 
